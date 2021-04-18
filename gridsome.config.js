2f33d29a88086dde5d65b6144c95bd34a1fd75eb
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2021-04-18 10:38:55 +0300

    Initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,33 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+
+module.exports = {
+  siteName: 'Gridsome',
+  runtimeCompiler: true,
+  plugins: [
+    {
+      use: '@meeg/gridsome-source-kentico-kontent',
+      options: {
+        deliveryClientConfig: {
+          projectId: process.env.KENTICO_KONTENT_PROJECT_ID
+        }
+      }
+    },
+	{
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-XXXXXXXXX-X'
+      }
+    }
+  ],
+  templates: {
+    Article: '/article/:slug',
+    ContentPage: '/:slug',
+    Tag: '/tags/:slug',
+    Category: '/categories/:slug'
+  }
+}
